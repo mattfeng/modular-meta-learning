5578c6ee783d9cb33f87b3438dc0321366fae75a
Matthew Feng <mattfeng@mit.edu>
2018-09-27 10:35:39 -0400

    Move model to CUDA

---
+++ pixel-mnist/model.py
@@ -0,0 +1,42 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
+from torch.autograd import Variable
+
+transforms = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.1307, ), (0.3081, ))
+]) # initial transform to normalize the data
+
+train_loader = torch.utils.data.DataLoader(
+    datasets.MNIST('./data', train=True, download=True,
+        transform=transforms),
+    batch_size=64, shuffle=True
+) # serve batches of 64 of MNIST
+
+test_loader = torch.utils.data.DataLoader(
+    datasets.MNIST('./data', train=False, transform=transforms),
+    batch_size=64, shuffle=True
+)
+
+class Pixel10Net(nn.Module):
+    def __init__(self, image_size, pixels):
+        super(Pixel10Net, self).__init__()
+        self.img_size = image_size
+        self.pixels = pixels
+
+        self.fc0 = nn.Linear(image_size, 1000)
+        self.fc1 = nn.Linear(1000, 50)
+        self.fc2 = nn.Linear(50, 10)
+    
+    def forward(self, x):
+        x = x.view(-1, self.img_size) # reshape x into 1 x img_size
+        x = F.relu(self.fc0(x))
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        return F.log_softmax(x)
+
+model = Pixel10Net(image_size=28 * 28)
+model.cuda()
---
+++ policy-sketch/craft-steps.py
@@ -0,0 +1,34 @@
+import numpy as np
+import itertools
+import random
+
+def inbounds(loc, R, C):
+    return 0 <= loc[0] < R and 0 <= loc[1] < C
+
+def neighbors(r, c, R, C):
+    cur = np.array([r, c])
+    offsets = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+    offsets = map(np.array, offsets)
+    return [(cur + offset) for offset in offsets 
+            if inbounds(cur + offset, R, C)]
+
+def main():
+    R, C = 10, 10
+    gr = 9
+    gc = 9
+
+    steps = 0
+    T = 1000
+    for t in range(T):
+        r = 4
+        c = 4
+        print(f"Trial: {t}")
+        while (r, c) != (gr, gc):
+            r, c = random.choice(neighbors(r, c, R, C))
+            steps += 1
+    
+    print(f"Average steps taken: {steps / T}")
+
+
+if __name__ == "__main__":
+    main()
